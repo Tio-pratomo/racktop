44f26f28da9fc4eb1363a06b8843a967edc6d9f2
tio <sulistiopratomo@gmail.com>
2020-08-19 22:46:43 +0700

    update jquery for signup

--- javascript/script.js
+++ javascript/script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $('.icon').click(function () {
         $('.navbar').slideToggle(1000);
     });
+
+    $('.navbar li:nth-child(3)').click(function () {
+        $('.modal').slideDown(500);
+    });
+
+    $('.close').click(function () {
+        $('.modal').hide('fast');
+    });
 });
