04b0e586c498c799b89bf8630b8f7ab8196fc694
tio <sulistiopratomo@gmail.com>
2020-08-27 02:17:18 +0700

    add modal for image if users click it

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.icon').click(function () {
-        $('.navbar').slideToggle(1000);
+        $('.navbar').slideToggle(700);
     });
 
     $('.navbar li:nth-child(3)').click(function () {
@@ -9,5 +9,51 @@ $(document).ready(function () {
 
     $('.close').click(function () {
         $('.modal').hide('fast');
+    });
+
+    const products = [$('.product-content1'), $('.product-content2'), $('.product-content3'), $('.product-content4')];
+
+    products.forEach(function (product) {
+        $(product).hover(function () {
+            $(this).css({
+                'transform': 'scale(1.1)'
+            });
+        }, function () {
+            $(this).css({
+                'transform': 'none'
+            });
+        });
+    });
+
+    products[0].click(function () {
+        $('.mod-img-1').show(400);
+    });
+
+    products[1].click(function () {
+        $('.mod-img-2').show(400);
+    });
+
+    products[2].click(function () {
+        $('.mod-img-3').show(400);
+    });
+
+    products[3].click(function () {
+        $('.mod-img-4').show(400);
+    });
+
+    $('.close-modal-image').click(function () {
+        $('.mod-img-1').hide(400);
+    });
+
+    $('.close-modal-image').click(function () {
+        $('.mod-img-2').hide(400);
+    });
+
+    $('.close-modal-image').click(function () {
+        $('.mod-img-3').hide(400);
+    });
+
+    $('.close-modal-image').click(function () {
+        $('.mod-img-4').hide(400);
     });
 });
