dba098750840c24e3c4b916c00e83f0e91bcd738
tio <sulistiopratomo@gmail.com>
2021-02-13 23:16:38 +0700

    memperbaiki elemen pada gambar dan menambah favicon

--- javascript/script.js
+++ javascript/script.js
@@ -11,49 +11,27 @@ $(document).ready(function () {
         $('.modal').hide('fast');
     });
 
-    const products = [$('.product-content1'), $('.product-content2'), $('.product-content3'), $('.product-content4')];
-
+    const products = [
+        $('.product-content1'),
+        $('.product-content2'),
+        $('.product-content3'),
+        $('.product-content4'),
+    ];
     products.forEach(function (product) {
-        $(product).hover(function () {
-            $(this).css({
-                'transform': 'scale(1.1)'
-            });
-        }, function () {
-            $(this).css({
-                'transform': 'none'
-            });
-        });
+        $(product).hover(
+            function () {
+                $(this).css({
+                    transform: 'scale(1.1)',
+                });
+            },
+            function () {
+                $(this).css({
+                    transform: 'none',
+                });
+            }
+        );
     });
 
-    products[0].click(function () {
-        $('.mod-img-1').show(400);
-    });
-
-    products[1].click(function () {
-        $('.mod-img-2').show(400);
-    });
-
-    products[2].click(function () {
-        $('.mod-img-3').show(400);
-    });
-
-    products[3].click(function () {
-        $('.mod-img-4').show(400);
-    });
-
-    $('.close-modal-image').click(function () {
-        $('.mod-img-1').hide(400);
-    });
-
-    $('.close-modal-image').click(function () {
-        $('.mod-img-2').hide(400);
-    });
-
-    $('.close-modal-image').click(function () {
-        $('.mod-img-3').hide(400);
-    });
-
-    $('.close-modal-image').click(function () {
-        $('.mod-img-4').hide(400);
-    });
+    const years = new Date();
+    $('.copyright span').text(years.getFullYear());
 });
